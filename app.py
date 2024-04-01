d6edd89e0e6fdb217ba3e9e8e3efffce72425926
UmeshChandraBathula <umeshbathula212219@gmail.com>
2024-04-01 17:56:25 +0530

    added the instructor embeddings took 12 min to embedd a 2 page document on my PC

--- app.py
+++ app.py
@@ -1,16 +1,82 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+import time
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text): 
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key ="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vectorstore.as_retriever(),
+        memory = memory
+    )
+    return conversation_chain
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon = ":books")
 
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents")
 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
-    
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files= True) 
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdfs text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                get_text_chunksstart_time = time.time()
+                text_chunks = get_text_chunks(raw_text)
+                get_text_chunksend_time = time.time()
+                execution_time = get_text_chunksend_time - get_text_chunksstart_time
+                print("Execution time:", execution_time, "seconds")
+                # create vector store
+                get_vectorstoresstart_time = time.time()
+                vectorstore = get_vectorstore(text_chunks)
+                get_vectorstoressend_time = time.time()
+                execution_time = get_vectorstoressend_time - get_vectorstoresstart_time
+                print("Execution time:", execution_time, "seconds")
+                
+                #CREATE CONVERSATION CHAIN
+                #st.session_state.conversation = get_conversation_chain(vectorstore)
+         
+                 
 
 if __name__ == "__main__":
     main()
